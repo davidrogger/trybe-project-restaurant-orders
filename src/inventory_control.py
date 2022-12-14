483559e9a7ac4574aa31f0f3c36c499dadb1d98d
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-14 17:51:25 -0300

    feat: created need to buy attribute

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -28,6 +28,16 @@ class InventoryControl:
         "frango": 50,
     }
 
+    NEED_TO_BUY = {
+        "pao": 0,
+        "carne": 0,
+        "queijo": 0,
+        "molho": 0,
+        "presunto": 0,
+        "massa": 0,
+        "frango": 0,
+    }
+
     def __init__(self):
         self.track_orders = TrackOrders()
 
@@ -37,13 +47,20 @@ class InventoryControl:
             if quantity == 0:
                 raise ValueError("Insufficient quantity")
 
-    def check_stock(self, order):
+    def consuming_inventory(self, ingredients):
+        for ingredient in ingredients:
+            self.CURRENT_INVENTORY[ingredient] -= 1
+            self.NEED_TO_BUY[ingredient] += 1
+
+    def inventory_moviment(self, order):
         ingredients = self.INGREDIENTS[order]
 
         self.check_ingredients_available(ingredients)
+        self.consuming_inventory(ingredients)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.inventory_moviment(order)
+        self.track_orders.add_new_order(customer, order, day)
 
     def get_quantities_to_buy(self):
-        pass
+        return self.NEED_TO_BUY
