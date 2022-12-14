0a3ddf961a20194b865e0482b0044e53a0720a29
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-14 15:19:16 -0300

    feat: initial method to add a new order

--- src/track_orders.py
+++ src/track_orders.py
@@ -5,8 +5,22 @@ class TrackOrders:
     def __len__(self):
         return len(self.customers)
 
-    def add_new_order(self, customer, order, day):
-        pass
+    def updated_customer_orders(self, customer, order):
+        orders = self.customers[customer]["orders"]
+
+        if order not in orders:
+            orders[order] = 1
+        else:
+            orders[order] += 1
+
+    def add_new_order(self, customer: str, order: set, day: int):
+        if customer not in self.customers:
+            self.customers[customer] = dict()
+            self.customers[customer]['orders'] = dict()
+            self.customers[customer]['days'] = set()
+
+        self.updated_customer_orders(customer, order)
+        self.customers[customer]['days'].add(day)
 
     def get_most_ordered_dish_per_customer(self, customer):
         pass
