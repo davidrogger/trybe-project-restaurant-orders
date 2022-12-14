d54d1b1eb15ff9c39aac00b1b7059801712f2a94
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-14 15:39:37 -0300

    feat: added most ordered dish by customer

--- src/track_orders.py
+++ src/track_orders.py
@@ -5,6 +5,17 @@ class TrackOrders:
     def __len__(self):
         return len(self.customers)
 
+    def update_most_ordered_dish(self, customer, order):
+        customer_data = self.customers[customer]
+
+        current_most_ordered = self.customers[customer]["most_ordered"]
+        current_most_ordered_qt = customer_data["orders"][current_most_ordered]
+
+        new_order_qt = customer_data["orders"][order]
+
+        if new_order_qt > current_most_ordered_qt:
+            self.customers[customer]["most_ordered"] = order
+
     def updated_customer_orders(self, customer, order):
         orders = self.customers[customer]["orders"]
 
@@ -13,17 +24,20 @@ class TrackOrders:
         else:
             orders[order] += 1
 
+        self.update_most_ordered_dish(customer, order)
+
     def add_new_order(self, customer: str, order: set, day: int):
         if customer not in self.customers:
             self.customers[customer] = dict()
-            self.customers[customer]['orders'] = dict()
-            self.customers[customer]['days'] = set()
+            self.customers[customer]["orders"] = dict()
+            self.customers[customer]["days"] = set()
+            self.customers[customer]["most_ordered"] = order
 
         self.updated_customer_orders(customer, order)
         self.customers[customer]['days'].add(day)
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        return self.customers[customer]["most_ordered"]
 
     def get_never_ordered_per_customer(self, customer):
         pass
